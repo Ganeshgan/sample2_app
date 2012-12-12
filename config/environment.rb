92c869501f08f7ada1d13cde6317fbab9c9a7602
Ganesh <ganesh@Blackjack.(none)>
2012-12-12 19:38:38 +0530

    Signin With Devise Completed

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 SampleApp::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :address => "smtp.gmail.com",
+  :port => 587,
+  :domain => 'invoscape.com',
+  :user_name => 'ganesh.newbie@invoscape.com',
+  :password => 'Fruit12butter',
+  :authentication => 'plain',
+  :enable_starttls_auto => true
+  }
+ActionMailer::Base.delivery_method = :smtp
